9239569ef65bac996add263d7d2135504d0af74e
Raffaello Bonghi <raffaello@rnext.it>
2016-09-25 17:20:46 +0200

    New definition on serial port

---
+++ orbus_interface/src/hardware/serial_controller.cpp
@@ -0,0 +1,11 @@
+#include "hardware/serial_controller.h"
+
+namespace orbus
+{
+
+serial_controller::serial_controller()
+{
+
+}
+
+}
---
+++ orbus_interface/src/hw_interface.cpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2016 Officine Robotiche
+ * Author: Raffaello Bonghi
+ * email:  raffaello@rnext.it
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU Lesser General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+ */
+
+#include <ros/ros.h>
+
+int main(int argc, char **argv) {
+
+    ros::init(argc, argv, "unav_interface");
+    ros::NodeHandle nh, private_nh("~");
+
+    ROS_INFO("A");
+
+    return 0;
+}

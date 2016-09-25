50222d23883eeb60aa366b6e255ee3333086fd52
Raffaello Bonghi <raffaello@rnext.it>
2016-09-25 20:32:05 +0200

    Remove all old object. Added a function to read a list of frame

--- orbus_interface/include/hardware/serial_controller.h
+++ orbus_interface/include/hardware/serial_controller.h
@@ -46,8 +46,50 @@ protected:
      */
     void* run();
 
+    /**
+     * @brief sendSerialFrame
+     * @param list_send
+     * @return
+     */
+    bool sendSerialFrame(vector<packet_information_t> list_send);
+    /**
+     * @brief sendSerialPacket
+     * @param packet
+     * @return
+     */
+    packet_t sendSerialPacket(packet_t packet);
+
 private:
-    bool send(packet_t packet);
+    /**
+     * @brief writePacket Send a packet from serial
+     * In a packet we have more messages. A typical data packet
+     * have this structure:
+     * -------------------------- ---------------------------- -----------------------
+     * | Length | CMD | DATA ... | Length | CMD | INFORMATION |Length | CMD | ... ... |
+     * -------------------------- ---------------------------- -----------------------
+     *    1        2 -> length    length+1 length+2 length+3   ...
+     * It is possible to have different type of messages:
+     * * Message with data (D)
+     * * Message with state information:
+     *      * (R) request data
+     *      * (A) ack
+     *      * (N) nack
+     * We have three parts to elaborate and send a new packet (if required)
+     * 1. [SAVING] The first part of this function split packets in a
+     * list of messages to compute.
+     * 2. [COMPUTE] If message have a data in tail, start compute and return a
+     * new ACK or NACK message to append in a new packet. If is a request
+     * message (R), the new message have in tail the data required.
+     * 3. [SEND] Encoding de messages and transform in a packet to send.
+     * @param packet the packet to send
+     * @return if well written return true
+     */
+    bool writePacket(packet_t packet);
+    /**
+     * @brief readPacket
+     * @return if received all data in packet return true
+     */
+    bool readPacket();
 
 private:
     // Serial port object
@@ -63,8 +105,11 @@ private:
 
     // The packet received from serial
     packet_t mReceive;
-
+    // buffer to send in Tx transimssion
     unsigned char BufferTx[MAX_BUFF_TX];
+
+    /// List to send messages to serial
+    vector<packet_information_t> list_send;
 };
 
 }

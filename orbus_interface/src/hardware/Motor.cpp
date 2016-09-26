a2f42c31064d5bf4bdbcd5363dc4c9f3d4f260d9
Raffaello Bonghi <raffaello@rnext.it>
2016-09-26 19:59:12 +0200

    Added function to initialize Hardware interface

--- orbus_interface/src/hardware/Motor.cpp
+++ orbus_interface/src/hardware/Motor.cpp
@@ -1,12 +1,113 @@
+
 #include "hardware/Motor.h"
 
-Motor::Motor(unsigned int number) : DiagnosticTask("motor_" + to_string(number) + "_status")
+#include <hardware_interface/joint_state_interface.h>
+#include <hardware_interface/joint_command_interface.h>
+
+#include <joint_limits_interface/joint_limits_urdf.h>
+#include <joint_limits_interface/joint_limits_rosparam.h>
+
+namespace ORController
 {
+
+Motor::Motor(const ros::NodeHandle& nh, orbus::serial_controller *serial, unsigned int number)
+    : DiagnosticTask("motor_" + to_string(number) + "_status")
+    , mNh(nh)
+    , mSerial(serial)
+{
+    command.bitset.motor = number;
     mName = "motor_" + to_string(number);
 
 }
 
+void Motor::registerControlInterfaces(hardware_interface::JointStateInterface joint_state_interface, hardware_interface::VelocityJointInterface velocity_joint_interface, shared_ptr<urdf::ModelInterface> urdf)
+{
+    /// Joint hardware interface
+    hardware_interface::JointStateHandle joint_state_handle(mName, &position, &velocity, &effort);
+
+    joint_state_interface.registerHandle(joint_state_handle);
+
+    /// Differential drive interface
+    hardware_interface::JointHandle joint_handle(joint_state_handle, &velocity_command);
+    velocity_joint_interface.registerHandle(joint_handle);
+
+    setupLimits(joint_handle, urdf);
+}
+
+void Motor::setupLimits(hardware_interface::JointHandle joint_handle, shared_ptr<urdf::ModelInterface> urdf)
+{
+    /// Add a velocity joint limits infomations
+    /// Populate with any of the methods presented in the previous example...
+    joint_limits_interface::JointLimits limits;
+    joint_limits_interface::SoftJointLimits soft_limits;
+
+    // Manual value setting
+    limits.has_velocity_limits = true;
+    limits.max_velocity = 5.0;
+
+    // Populate (soft) joint limits from URDF
+    // Limits specified in URDF overwrite existing values in 'limits' and 'soft_limits'
+    // Limits not specified in URDF preserve their existing values
+    if(urdf != NULL) {
+        boost::shared_ptr<const urdf::Joint> urdf_joint = urdf->getJoint(mName);
+        const bool urdf_limits_ok = getJointLimits(urdf_joint, limits);
+        const bool urdf_soft_limits_ok = getSoftJointLimits(urdf_joint, soft_limits);
+        if(urdf_limits_ok) {
+            ROS_INFO_STREAM("LOAD " << mName << " limits from URDF: " << limits.max_velocity << " rad/s");
+        }
+        if(urdf_soft_limits_ok) {
+            ROS_INFO_STREAM("LOAD " << mName << " soft limits from URDF: " << limits.max_velocity << " rad/s");
+        }
+    }
+
+    // Populate (soft) joint limits from the ros parameter server
+    // Limits specified in the parameter server overwrite existing values in 'limits' and 'soft_limits'
+    // Limits not specified in the parameter server preserve their existing values
+//    const bool rosparam_limits_ok = getJointLimits(mName, mNh, limits);
+//    if(rosparam_limits_ok) {
+//        ROS_INFO_STREAM("LOAD " << mName << " limits from ROSPARAM: " << limits.max_velocity << " rad/s");
+//    }
+
+    // Send joint limits information to board
+    motor_t constraint;
+    constraint.position = -1;
+    constraint.velocity = (motor_control_t) limits.max_velocity*1000;
+    constraint.current = -1;
+    // Set type of command
+    command.bitset.command = MOTOR_CONSTRAINT;
+
+    // Build a packet
+    message_abstract_u temp;
+    temp.motor.motor = constraint;
+    packet_information_t frame = CREATE_PACKET_DATA(command.command_message, HASHMAP_MOTOR, temp);
+    // Add packet in the frame
+    mSerial->addFrame(frame);
+
+    joint_limits_interface::VelocityJointSoftLimitsHandle handle(joint_handle, // We read the state and read/write the command
+                                                                 limits,       // Limits spec
+                                                                 soft_limits);  // Soft limits spec
+
+    vel_limits_interface.registerHandle(handle);
+}
+
 void Motor::run(diagnostic_updater::DiagnosticStatusWrapper &stat)
 {
+
+}
+
+
+void Motor::motorFrame(unsigned char option, unsigned char type, unsigned char command, message_abstract_u message)
+{
+    ROS_DEBUG_STREAM("Motor decode " << mName );
+    switch(command)
+    {
+        case MOTOR_MEASURE:
+
+        break;
+        case MOTOR_VEL_PID:
+        ROS_INFO_STREAM("New PID parameter");
+        break;
+    }
+}
 
 }
--- orbus_interface/src/hardware/uNavController.cpp
+++ orbus_interface/src/hardware/uNavController.cpp
@@ -1,20 +1,43 @@
 
 #include <string>
 
+
+#include <hardware_interface/joint_state_interface.h>
+#include <hardware_interface/joint_command_interface.h>
+
+
 #include "hardware/uNavController.h"
 
 namespace ORController
 {
 
-uNavController::uNavController(orbus::serial_controller *serial) : GenericController(serial)
+uNavController::uNavController(const ros::NodeHandle &nh, orbus::serial_controller *serial) : GenericController(nh, serial)
 {
     /// Added all callback to receive information about messages
-    bool initCallback = mSerial->addCallback(&uNavController::motorFrame, this, HASHMAP_MOTOR);
+    bool initCallback = mSerial->addCallback(&uNavController::allMotorsFrame, this, HASHMAP_MOTOR);
 
     // Add Two motor
-    list_motor.push_back(new Motor(1));
-    list_motor.push_back(new Motor(2));
+    list_motor.push_back(new Motor(nh, serial, 0));
+    list_motor.push_back(new Motor(nh, serial, 1));
 
+}
+
+void uNavController::registerControlInterfaces()
+{
+    ROS_INFO_STREAM("Register Control interfaces");
+    for(unsigned i=0; i < list_motor.size(); ++i)
+    {
+        // Get motor
+        Motor* motor_obj = list_motor.at(i);
+        // Register interface
+        motor_obj->registerControlInterfaces(joint_state_interface, velocity_joint_interface, urdf);
+    }
+
+    mSerial->start();
+
+    /// Register interfaces
+    registerInterface(&joint_state_interface);
+    registerInterface(&velocity_joint_interface);
 }
 
 void uNavController::updateJointsFromHardware()
@@ -27,8 +50,20 @@ void uNavController::writeCommandsToHardware(ros::Duration period)
 
 }
 
-void uNavController::motorFrame(unsigned char option, unsigned char type, unsigned char command, message_abstract_u message) {
+void uNavController::allMotorsFrame(unsigned char option, unsigned char type, unsigned char command, message_abstract_u message)
+{
     ROS_INFO_STREAM("uNav Controller I'm here");
+    motor_command_map_t motor;
+    motor.command_message = command;
+    int number_motor = (int) motor.bitset.motor;
+    if(number_motor < list_motor.size())
+    {
+        // Get motor
+        Motor* motor_obj = list_motor.at(number_motor);
+        // Update information
+        motor_obj->motorFrame(option, type, motor.bitset.command, message);
+    }
+
 }
 
 }

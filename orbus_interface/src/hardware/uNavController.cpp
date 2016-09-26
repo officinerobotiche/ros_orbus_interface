e9b55109319d29e41a884566b5f27489eaf48497
Raffaello Bonghi <raffaello@rnext.it>
2016-09-26 20:21:42 +0200

    minor fixes

--- orbus_interface/src/hardware/uNavController.cpp
+++ orbus_interface/src/hardware/uNavController.cpp
@@ -16,9 +16,15 @@ uNavController::uNavController(const ros::NodeHandle &nh, orbus::serial_controll
     /// Added all callback to receive information about messages
     bool initCallback = mSerial->addCallback(&uNavController::allMotorsFrame, this, HASHMAP_MOTOR);
 
-    // Add Two motor
-    list_motor.push_back(new Motor(nh, serial, 0));
-    list_motor.push_back(new Motor(nh, serial, 1));
+    //Initialize motors
+    Motor motor0(nh, serial, 0);
+    Motor motor1(nh, serial, 1);
+    // Add Two motors on list
+    list_motor.push_back(motor0);
+    list_motor.push_back(motor1);
+
+    /// Register all control interface avaiable
+    registerControlInterfaces();
 
 }
 
@@ -28,12 +34,13 @@ void uNavController::registerControlInterfaces()
     for(unsigned i=0; i < list_motor.size(); ++i)
     {
         // Get motor
-        Motor* motor_obj = list_motor.at(i);
+        Motor motor_obj = list_motor.at(i);
         // Register interface
-        motor_obj->registerControlInterfaces(joint_state_interface, velocity_joint_interface, urdf);
+        motor_obj.registerControlInterfaces(joint_state_interface, velocity_joint_interface, urdf);
     }
-
-    mSerial->start();
+    ROS_INFO_STREAM("Send all configuration");
+    // Send list of Command
+    mSerial->sendList();
 
     /// Register interfaces
     registerInterface(&joint_state_interface);
@@ -52,16 +59,21 @@ void uNavController::writeCommandsToHardware(ros::Duration period)
 
 void uNavController::allMotorsFrame(unsigned char option, unsigned char type, unsigned char command, message_abstract_u message)
 {
-    ROS_INFO_STREAM("uNav Controller I'm here");
+
     motor_command_map_t motor;
     motor.command_message = command;
     int number_motor = (int) motor.bitset.motor;
+    ROS_INFO_STREAM("Frame [Option: " << option << ", HashMap: " << type << ", Nmotor: " << number_motor << ", Command: " << (int) motor.bitset.command << "]");
     if(number_motor < list_motor.size())
     {
         // Get motor
-        Motor* motor_obj = list_motor.at(number_motor);
+        Motor motor_obj = list_motor.at(number_motor);
         // Update information
-        motor_obj->motorFrame(option, type, motor.bitset.command, message);
+        motor_obj.motorFrame(option, type, motor.bitset.command, message);
+    }
+    else
+    {
+        ROS_WARN_STREAM("Error enything motor is initialized for Motor: " << number_motor);
     }
 
 }
